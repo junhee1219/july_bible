d662ca80d40c245280477a76c00cb72b2eb6dd1d
JHLee <leejunhee1219@snu.ac.kr>
2023-07-01 23:40:25 +0900

    line break

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 
     // 테이블 행 클릭 시 modal 창 표시
     $('td').click(function () {
-        
+
         //var date = $(this).parent('tr').data('date');
         var chapter = $(this).parent('tr').find('td:nth-child(2)').text();
         var book = $(this).closest('.chapter').find('h2').text();
@@ -17,28 +17,26 @@ $(document).ready(function () {
     });
 
     // modal 창 닫기
-    $('#close-btn, #modal').on('click', function(event) {
+    $('#close-btn, #modal').on('click', function (event) {
         if (event.target.id === 'modal') {
-          closeModal();
+            closeModal();
         }
-      });
+    });
 
-      function closeModal() {
+    function closeModal() {
         $('#modal').hide();
-      }
+    }
 
     function getBibleChapterContent(book, chapter) { // ex 골로새서 1장
-        let filename = book+' '+chapter+".txt"
-        $.ajax({
-            url: filename,
-            dataType: 'text',
-            success: function(data) {
-                $('#chapter-content').text(data);
-            },
-            error: function(xhr, status, error) {
-              console.log('파일을 읽어오는 중 에러 발생:', error);
+        let filename = book + ' ' + chapter + ".txt"
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', filename, true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                var contents = xhr.responseText;
+                $('#chapter-content').text(contents);
             }
-          });
-        
+        };
+        xhr.send();
     }
 });
