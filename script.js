40d4fc28e2ce6426643da45d2bb413b3413d7659
JHLee <leejunhee1219@snu.ac.kr>
2023-07-08 00:20:19 +0900

    edit service-worker.js

--- script.js
+++ script.js
@@ -10,30 +10,7 @@ if ('serviceWorker' in navigator) {
       });
   });
 }
-// 필요한 파일 캐싱
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('my-cache').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/styles.css',
-        '/script.js',
-        '/index.html'
-      ]);
-    })
-  );
-});
 
-// 오프라인 요청 대응
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    }).catch(function() {
-      return caches.match('/index.html');
-    })
-  );
-});
 
 
 $(document).ready(function () {
